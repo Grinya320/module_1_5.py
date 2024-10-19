918c33161aa4b0c473e13e3db6d80f53ce363306
Grinya320 <grilya320@gmail.com>
2024-10-19 18:52:42 +0300

    Add files via upload

---
+++ module_1_5.py
@@ -0,0 +1,7 @@
+immutable_var = (320, 'hero', 'vegetables', 687)
+print(immutable_var)
+immutable_var[0] = 900
+mutable_list = [345, 'mutant', 963, 'hell']
+print(mutable_list)
+mutable_list[0] = 944
+print(mutable_list)
