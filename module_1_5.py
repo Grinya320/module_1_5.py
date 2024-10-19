11e6f41bce8145df9ff6028b3f53f9125e61945a
Grinya320 <grilya320@gmail.com>
2024-10-19 18:56:31 +0300

    Update module_1_5.py

--- module_1_5.py
+++ module_1_5.py
@@ -1,7 +1,6 @@
 immutable_var = (320, 'hero', 'vegetables', 687)
 print(immutable_var)
-immutable_var[0] = 900
 mutable_list = [345, 'mutant', 963, 'hell']
 print(mutable_list)
-mutable_list[0] = 944
+mutable_list[1] = 'unluck'
 print(mutable_list)
